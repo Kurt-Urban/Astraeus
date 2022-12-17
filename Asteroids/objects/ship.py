ab3cfbc1fa432485cb8563bf19aed03fbe3d570e
Kurt-Urban <kurturban00@gmail.com>
2022-12-17 20:44:11 +0100

    feat: ship thruster animation

--- Asteroids/objects/ship.py
+++ Asteroids/objects/ship.py
@@ -6,8 +6,13 @@ from Asteroids.utils.object_functions import movement, screen_wrap
 class Ship(pygame.sprite.Sprite):
     def __init__(self, xy, screen):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("assets/ship.png")
-        self.image = pygame.transform.scale(self.image, (35, 35))
+        img1 = pygame.image.load("assets/ship1.png")
+        img1 = pygame.transform.scale(img1, (30, 57))
+        img2 = pygame.image.load("assets/ship2.png")
+        img2 = pygame.transform.scale(img2, (30, 57))
+
+        self.images = [img1, img2]
+        self.image = self.images[0]
         self.screen = screen
         self.rotated = self.image
         self.rect = self.image.get_rect()
@@ -19,8 +24,9 @@ class Ship(pygame.sprite.Sprite):
         self.velocity = pygame.math.Vector2(self.speed, 0)
         self.position = pygame.math.Vector2(xy, xy)
         self.shot = False
-        self.shooting_delay_default = 20
+        self.shooting_delay_default = 200
         self.shooting_delay = self.shooting_delay_default
+        self.moving = False
 
     def update(self):
         # Movement Logic
@@ -29,7 +35,6 @@ class Ship(pygame.sprite.Sprite):
             self.speed *= 0.98
         else:
             self.speed = 0
-
         movement(self, acceleration=0.985, ship=True)
         screen_wrap(self)
         self.get_key_press()
@@ -38,7 +43,12 @@ class Ship(pygame.sprite.Sprite):
         if self.shot and self.shooting_delay != 0:
             self.shooting_delay -= 1
 
+        self.image = pygame.transform.rotate(
+            self.images[1 if self.moving else 0], -self.heading
+        )
+
     def foward(self):
+        self.moving = True
         self.speed += 0.3
         if self.speed >= self.top_speed:
             self.speed = self.top_speed
@@ -46,18 +56,22 @@ class Ship(pygame.sprite.Sprite):
 
     def rotate(self, angle):
         if self.heading >= 360:
-            self.heading = -360
+            self.heading -= 360
         if self.heading <= -360:
             self.heading += 360
 
         self.heading += angle
-        self.image = pygame.transform.rotate(self.rotated, -self.heading)
+        self.image = pygame.transform.rotate(
+            self.images[1 if self.moving else 0], -self.heading
+        )
         self.rect = self.image.get_rect(center=self.rect.center)
 
     def get_key_press(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.foward()
+        else:
+            self.moving = False
         if keys[pygame.K_a]:
             self.rotate(-self.rotate_speed)
         if keys[pygame.K_d]:
